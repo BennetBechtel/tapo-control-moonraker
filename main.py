665b17b865296ba9f9cef342a46c82b980fb860e
Bennet Bechtel <bennet.bechtel007@gmail.com>
2026-01-22 18:49:06 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
+
+port = int(os.getenv("PORT", 56427))  # Default to 56427 if PORT is not set
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="localhost", port=port, reload=True)
---
+++ server.py
@@ -0,0 +1,58 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from tapo import ApiClient
+
+load_dotenv()
+
+
+def require_env(name: str) -> str:
+    value = os.getenv(name)
+    if value is None:
+        raise RuntimeError(f"Missing required environment variable: {name}")
+    return value
+
+
+TAPO_USERNAME = require_env("TAPO_USERNAME")
+TAPO_PASSWORD = require_env("TAPO_PASSWORD")
+TAPO_IP_ADDRESS = require_env("TAPO_IP_ADDRESS")
+
+
+app = FastAPI()
+client = ApiClient(tapo_username=TAPO_USERNAME, tapo_password=TAPO_PASSWORD)
+# device needs to be initialized every time else you get an Exception: Tapo(SessionTimeout)
+
+
+@app.get("/status")
+async def status():
+    device = await client.p100(ip_address=TAPO_IP_ADDRESS)
+    if device is None:
+        raise HTTPException(status_code=500, detail="Device not initialized")
+
+    device_info = await device.get_device_info()
+    device_info = device_info.to_dict()
+    is_power_on = device_info.get("device_on")
+
+    status = "on" if is_power_on else "off"
+    return {"status": status}
+
+
+@app.get("/on")
+async def on():
+    device = await client.p100(ip_address=TAPO_IP_ADDRESS)
+    if device is None:
+        raise HTTPException(status_code=500, detail="Device not initialized")
+
+    await device.on()
+    return {"status": "on"}
+
+
+@app.get("/off")
+async def off():
+    device = await client.p100(ip_address=TAPO_IP_ADDRESS)
+    if device is None:
+        raise HTTPException(status_code=500, detail="Device not initialized")
+
+    await device.off()
+    return {"status": "off"}
